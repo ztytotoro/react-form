832ae3b5777a9f9e35a6d3da52e192c842f91d8d
Yarn <ztytotoro@users.noreply.github.com>
2019-12-29 23:38:48 +0800

    update

--- react-form/src/components/control.tsx
+++ react-form/src/components/control.tsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeFactory } from '../utils';
 
 const [register, get] = makeFactory();
 
 export interface ControlProps<T extends any> {
-  value: T;
-  onChange(): T;
+    value: T;
+    onChange(): T;
+    validator?(value: T): boolean;
 }
 
 export type ControlFC<Props extends ControlProps<any>> = React.FC<Props>;
 
 export interface IControl<Props extends ControlProps<any>> {
-  name: string;
-  component: ControlFC<Props>;
+    name: string;
+    component: ControlFC<Props>;
 }
 
 export const InputControl: ControlFC<ControlProps<string>> = ({}) => {
-  return <input />;
+    const [value, setValue] = useState<string>('');
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setValue(event.target.value);
+    }
+    return <input value={value} onChange={handleChange} />;
 };
 
 interface FormProps {
-  controls: string[];
+    controls: string[];
 }
 
 export const Form: React.FC<FormProps> = ({ controls }) => {
-  return <div></div>;
+    return (
+        <div>
+            {controls.map(control => (
+                <div></div>
+            ))}
+        </div>
+    );
 };
